31782bf3e1d05bef7b51f98df08ec4de3f6ff6eb
Marina Kukso <marina.kukso@gmail.com>
2017-09-02 20:40:37 -1000

    +dolphin

--- cols/main.js
+++ cols/main.js
@@ -5,13 +5,15 @@ var normals = require('angle-normals')
 var planeMesh = require("grid-mesh")(300, 60)
 var camera = require('regl-camera')(regl, {
   center: [0,0,0],
-  distance: 25
+  distance: 25,
+  far: 500
 })
 var mat4 = require('gl-mat4')
 var vec3 = require('gl-vec3')
-var mat0 = [], v0 = [], pmat = []
+var mat0 = [], v0 = [], pmat = [], rmat = []
 var mesh = column({ radius: 2, height: 25 })
 var col = fromMesh(mesh)
+var golf = require('./golf.json')
 
 var batch = []
 for (var i = 0; i < 10; i++) {
@@ -167,6 +169,61 @@ function floor (regl) {
     elements: planeMesh.cells
   })
 }
+function dolphin (regl){
+  return regl({
+    frag: glsl`
+      precision mediump float;
+      #pragma glslify: snoise = require('glsl-noise/simplex/4d')
+      varying vec3 vnormal, vpos;
+      uniform float t;
+      void main () {
+        vec3 p = vnormal+0.2/(snoise(vec4(vpos*0.01,sin(t)+20.5))*0.5+0.3);
+        float cross = abs(max(
+          max(sin(p.z*10.0+p.y), sin(p.y*01.0)),
+          sin(p.x*10.0)
+          ));
+        gl_FragColor = vec4(p*cross, 1);
+      }`,
+    vert: glsl`
+      precision mediump float;
+      uniform mat4 model, projection, view;
+      attribute vec3 position, normal;
+      varying vec3 vnormal, vpos;
+      uniform float t;
+      void main () {
+        vnormal = normal;
+        vpos = position;
+        gl_Position = projection * view * model *
+        vec4(position, 1.0);
+        gl_PointSize = 10.0*sin(t);
+      }`,
+    attributes: {
+      position: golf.positions,
+      normal: normals(golf.cells, golf.positions)
+    },
+    elements: golf.cells,
+    uniforms: {
+      t: function(context, props){
+           return context.time
+         },
+      model: function(context, props){
+        var t = context.time
+        mat4.identity(rmat)
+        //mat4.scale(rmat, rmat,[0.1,0.1,0.1])
+        mat4.rotateY(rmat, rmat, -t)
+        mat4.rotateX(rmat, rmat, -t/2.0)
+        mat4.rotateZ(rmat, rmat, t)
+        return rmat
+      }
+    },
+    primitive: "triangles",
+    blend: {
+      enable: true,
+      func: { src: 'src alpha', dst: 'one minus src alpha' }
+    },
+    cull: { enable: true }
+  })
+}
 function bg (regl) {
   return regl({
     frag: glsl`
@@ -208,7 +265,8 @@ var draw = {
   bg: bg(regl),
   col: col,
   roof: roof(regl),
-  floor: floor(regl)
+  floor: floor(regl),
+  dolphin: dolphin(regl)
 }
 regl.frame(function () {
   regl.clear({ color: [0.9,0.9,0.9,1] })
@@ -217,5 +275,6 @@ regl.frame(function () {
     draw.col(batch)
     draw.roof({ location: [-250,20, -30] })
     draw.floor({ location: [-250,-10, -30] })
+    draw.dolphin()
   })
 })
