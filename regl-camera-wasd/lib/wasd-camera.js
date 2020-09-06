78046f551762b56bbf63e2c42ed7c2301292e466
marina <marina.kukso@gmail.com>
2020-09-06 12:33:26 -1000

    regl-camera-wasd: original mk wasd-camera added

--- regl-camera-wasd/lib/wasd-camera.js
+++ regl-camera-wasd/lib/wasd-camera.js
@@ -1,4 +1,5 @@
 var mat4 = require('gl-mat4')
+var vec3 = require('gl-vec3')
 var defined = require('defined')
 
 module.exports = Camera
@@ -13,15 +14,41 @@ function Camera (opts) {
     near: defined(opts.near, 0.01),
     far: defined(opts.far, 1000.0),
     width: defined(opts.width, 1),
-    height: defined(opts.height, 1)
+    height: defined(opts.height, 1),
+    playerpos: [0,0,0],
+    rotation: mat4.identity(new Float32Array(16))
  
   }
 }
+
+var fwd = [0,0,1]
+var bwd = [0,0,-1]
+var tmpv = [0,0,0]
+
+Camera.prototype.onkeydown = function (ev) {
+  if (ev.code === "KeyW") {
+    vec3.transformMat4(tmpv, fwd, this.props.rotation)
+    vec3.add(this.props.playerpos, this.props.playerpos, tmpv)
+  }
+  if (ev.code === "KeyS") {
+    vec3.transformMat4(tmpv, bwd, this.props.rotation)
+    vec3.add(this.props.playerpos, this.props.playerpos, tmpv)
+  }
+  if (ev.code === "KeyA") {
+    mat4.rotateY(this.props.rotation, this.props.rotation, -0.1)
+  }
+  if (ev.code === "KeyD") {
+    mat4.rotateY(this.props.rotation, this.props.rotation, 0.1)
+  }
+}
+
 Camera.prototype.update = function (dt) {
   var aspect = this.props.width / this.props.height
   mat4.perspective(this.props.projection, this.props.fovy,
     aspect, this.props.near, this.props.far)
   mat4.identity(this.props.view)
+  mat4.multiply(this.props.view, this.props.view, this.props.rotation)
+  mat4.translate(this.props.view, this.props.view, this.props.playerpos)
 }
 
 
